c50431cedce22129d96259503d42c3c69312eab8
Gustavo Kira <gustavokira@gmail.com>
2017-11-04 16:30:49 -0200

    estrategia aleatoria java

---
+++ core/src/ai/EstrategiaAleatoriaSimulacao.java
@@ -0,0 +1,33 @@
+package ai;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import core.Estrategia;
+import core.Info;
+import core.Movimento;
+
+public class EstrategiaAleatoriaSimulacao extends Estrategia{
+
+	public Movimento atual;
+	
+	@Override
+	public Movimento escolherMovimento(Info info, ArrayList<Movimento> movimentos) {
+		Random r = new Random();
+		int i = r.nextInt(movimentos.size());
+		Movimento m = movimentos.get(i);
+		this.atual = m;
+		return m;
+	}
+
+	@Override
+	public String getNome() {
+		return "aleatória";
+	}
+
+	@Override
+	public String getEquipe() {
+		return "simulador";
+	}
+
+}
